7b681f31380286a2512d3d04f9a7c98ad24c72cc
Rafael Sartori <rafael.sartori.vantin@gmail.com>
2023-06-02 18:32:15 -0300

    added example component popup

---
+++ src/components/popup.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Popup = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Popup</Text>
+        </View>
+    );
+}
+
+export default Popup;
